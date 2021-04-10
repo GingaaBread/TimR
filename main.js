89074c71bd6951cf9be37b0d9dc9acdf2e6544c2
none <none>
2021-04-10 21:42:30 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,61 @@
+let countActive = false;
+
+// Get the components
+const daysText = document.getElementById("days");
+const hoursText = document.getElementById("hours");
+const minutesText = document.getElementById("minutes");
+const secondsText = document.getElementById("seconds");
+const setupButton = document.getElementById("countdown-setup");
+const setupContainer = document.getElementById("countdown-popup-container");
+const cancelSetup = document.getElementById("cancel-setup");
+const countdownDate = document.getElementById("countdown-date");
+const countdownSetupForm = document.getElementById("countdown-setup-form");
+const countdownName = document.getElementById("countdown-name");
+const countdownNameField = document.getElementById("countdown-name-field");
+
+// Compute the dates
+let destinationDate;
+let currentDate;
+let currentName;
+
+let storageItem = localStorage.getItem("destinationDate");
+if (storageItem != null) {
+    destinationDate = new Date(JSON.parse(storageItem));
+    countActive = true;
+}
+
+let storageName = localStorage.getItem("name");
+if (storageName != null) {
+    countdownName.innerText = JSON.parse(storageName);
+}
+
+// Display time
+setInterval(e => {
+    if (countActive) {
+        currentDate = new Date();
+
+        if (destinationDate - currentDate >= 0) {
+            let difference = Math.abs(destinationDate - currentDate);
+            daysText.innerText = Math.floor(difference / (1000 * 60 * 60 * 24));
+            hoursText.innerText = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            minutesText.innerText = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            secondsText.innerText = Math.floor((difference % (1000 * 60)) / 1000);
+        } else {
+            countActive = false;
+        }
+    }
+}, 1000);
+
+// Enables opening and closing the setup menu
+setupButton.addEventListener("click", () => setupContainer.classList.replace("hidden", "active"));
+cancelSetup.addEventListener("click", () => setupContainer.classList.replace("active", "hidden"));
+
+countdownSetupForm.addEventListener("submit", e => {
+    e.preventDefault()
+    destinationDate = new Date(countdownDate.value);
+    countdownName.innerText = countdownNameField.value;
+    localStorage.setItem("destinationDate", JSON.stringify(destinationDate));
+    localStorage.setItem("name", JSON.stringify(countdownNameField.value))
+    setupContainer.classList.replace("active", "hidden");
+    countActive = true;
+});
